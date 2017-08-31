2b14870dc67fb445518fd71a0919e43facc25f2d
AkeRyuu <berezhnogo@gmail.com>
2017-08-31 17:34:43 +0300

    new styles, clear filter, sync statuses

--- public/app/app.js
+++ public/app/app.js
@@ -2,10 +2,11 @@ var socket = io({
     autoConnect: true
   });
 Vue.component('tasklist',{
-    template: '<ul><li v-for="(task,index) in data" v-if="check(task.status)">\
-    <status v-bind:status.sync="task.status"></status> | {{task.task}} | {{task.contact}} | \
+    template: '<ul class="tasklist"><li v-for="(task,index) in data" v-if="check(task.status)">\
+    <div class="user"><label>Користувач</label></div><status v-bind:status.sync="task.status"><taskTime v-bind:status="task.status" :startTime="task.startTime" :endTime="task.endTime"></taskTime></status><div class="task"><label>Задача</label><p>{{task.task}}</p></div>\
+    <div class="contact"><label>Контакти</label><p>{{task.contact}}</p></div>\
     <taskAction v-bind:status="task.status" v-on:change="changeStatus(index)"></taskAction>\
-    <taskTime v-bind:status="task.status" :startTime="task.startTime" :endTime="task.endTime"></taskTime>\
+    \
     </li></ul>',
     props: {
         data: Array,
@@ -17,6 +18,7 @@ Vue.component('tasklist',{
         },
         changeStatus: function(id) {
             this.data[id].status == "onStart" ? (this.data[id].status = "onProcess",this.data[id].startTime = new Date()) : (this.data[id].status = "onDone",this.data[id].endTime = new Date());
+            socket.emit("new status",{id:id,status:this.data[id].status});
         },
         addTask: function(obj) {
             this.data.push(obj);
@@ -37,14 +39,14 @@ Vue.component('status',{
     props: {
         status: String
     },
-    template: '<a v-bind:class="status" v-if="status == \'onStart\'" >New</a><a v-bind:class="status" v-else-if="status == \'onProcess\'">In process</a><a v-bind:class="status" v-else-if="status == \'onDone\'"">Completed</a>'
+    template: '<div class="status"><label>Статус</label><span v-bind:class="status" v-if="status == \'onStart\'" >New</span><span v-bind:class="status" v-else-if="status == \'onProcess\'">In process</span><span v-bind:class="status" v-else-if="status == \'onDone\'"">Completed</span><slot></slot></div>'
 })
 
 Vue.component('taskAction',{
     props: {
         status: String
     },
-    template: '<a v-bind:class="status"  v-if="status == \'onStart\'" v-on:click="changeStatus">Взяти</a><a v-bind:class="status" v-else-if="status == \'onProcess\'" v-on:click="changeStatus">Завершити</a><a v-bind:class="status" v-else-if="status == \'onDone\'">Завершено</a>',
+    template: '<div class="taskAction"><a v-bind:class="status"  v-if="status == \'onStart\'" v-on:click="changeStatus">Взяти</a><a v-bind:class="status" v-else-if="status == \'onProcess\'" v-on:click="changeStatus">Завершити</a><a v-bind:class="status" v-else-if="status == \'onDone\'">Завершено</a></div>',
     methods: {
         changeStatus: function() {
             this.$emit('change');
@@ -58,13 +60,30 @@ Vue.component('taskTime',{
         endTime: Date,
         status: String
     },
-    template: '<div v-if="status == \'onProcess\'">{{start}}</div><div v-else-if="status == \'onDone\'">{{start}} - {{end}}</div>',
-    computed: {
+    template: '<div v-if="start != null && end == null">{{start}}</div><div v-else-if="start != null && end != null">{{start}} - {{end}}</div>',
+    computed: { // -------------- dorobutu logiky !!!!
         start: function() {
-            return this.startTime.getDate()+"." + (this.startTime.getMonth()+1)+ "." + this.startTime.getFullYear();
+            if (this.startTime !=  undefined) {
+                if (this.startTime.getDate() == new Date().getDate() && this.startTime.getMonth() == new Date().getMonth()) {
+                    return this.startTime.getHours()+":"+this.startTime.getMinutes();
+                } else {
+                    return this.startTime.getDate()+"." + (this.startTime.getMonth()+1);
+                }
+            } else {
+                return null;
+            }
         },
         end: function() {
-            return this.endTime.getDate()+"." + (this.endTime.getMonth()+1)+ "." + this.endTime.getFullYear();
+            if (this.endTime !=  undefined) {
+                if (this.endTime.getDate() ==this.endTime.getDate() && this.endTime.getMonth() == this.startTime.getMonth()) {
+                    return this.endTime.getHours()+":"+this.endTime.getMinutes();
+                } else {
+                    return null;
+                }
+            } else {
+                return null;
+            }
+            //return this.endTime.getDate()+"." + (this.endTime.getMonth()+1)+ "." + this.endTime.getFullYear();
         }
     }
 })
@@ -84,6 +103,9 @@ Vue.component('modal', {
         submit: function() {
             (this.obj.task != '' && this.obj.contact != '') ? (this.data.push(this.obj),socket.emit('new task',this.obj)) : console.log("bad fields");
             this.$emit('close');
+        },
+        close: function(e) {
+            this.$emit('close');
         }
     }
   })
@@ -91,17 +113,37 @@ Vue.component('modal', {
 
 Vue.component('statusfilter',{
     data:function () {
-        return {filter: []}
+        return {
+            filter: [],
+            toggle: "true"
+        }
     },
-    template: '<div><input type="checkbox" @change="update" id="onStart" value="onStart" v-model="filter">\
-    <label for="onStart">New</label>\
+    template: '<div class="statusfilter"><div><input type="checkbox" @change="update" id="onStart" value="onStart" v-model="filter">\
+    <label for="onStart">New</label></div><div>\
     <input type="checkbox" @change="update" id="onProcess" value="onProcess" v-model="filter">\
-    <label for="onProcess">Processing</label>\
+    <label for="onProcess">Processing</label></div><div>\
     <input type="checkbox" @change="update" id="onDone" value="onDone" v-model="filter">\
-    <label for="onDone">Done</label></div>',
+    <label for="onDone">Done</label></div><div>\
+    <input type="checkbox" @click="clear" id="all" v-model="toggle" v-bind:true-value="true" v-bind:false-value="false">\
+    <label for="all" style="margin-left:25px;">Всі</label>\
+    </div></div>',
     methods: {
         update: function() {
             this.$emit('updatefilter',this.filter);
+        },
+        clear: function() {
+            this.toggle == true ? this.$emit('updatefilter',[]) : console.log("rty");
+            this.filter = [];
+        }
+    },
+    watch: {
+        filter: function(e) {
+            if (e.length == 0) {
+                this.toggle = true; 
+            }
+            else {
+                this.toggle = false;
+            }
         }
     }
 })
@@ -130,7 +172,7 @@ var app = new Vue({
     },
     created:function(){
         socket.on('connect',()=>{
-            socket.emit('new task',{contact:"dsasadsa",status:"onStart",task:"dfadsasad"})
+            // socket.emit('new task',{contact:"dsasadsa",status:"onStart",task:"dfadsasad"})
             socket.emit('get list',socket.id);
         })        
     }
@@ -140,6 +182,8 @@ socket.on('get list', function(list){
     app.taskList = list;
   });
 socket.on('new task', function(task){
-    console.log(task);
     app.taskList.push(task);
+})
+socket.on('new status', function(obj){
+    app.taskList[obj.id].status = obj.status;
 })
